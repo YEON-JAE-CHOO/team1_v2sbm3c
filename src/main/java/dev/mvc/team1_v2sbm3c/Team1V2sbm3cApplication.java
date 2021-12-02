c71eec9dc075486769bbf5aadd94eaeffaa7b8b1
choo <khy00026@gmail.com>
2021-12-02 19:47:52 +0900

    기본

--- src/main/java/dev/mvc/team1_v2sbm3c/Team1V2sbm3cApplication.java
+++ src/main/java/dev/mvc/team1_v2sbm3c/Team1V2sbm3cApplication.java
@@ -1,16 +1,15 @@
 package dev.mvc.team1_v2sbm3c;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@ComponentScan(basePackages = {"dev.mvc"})
 public class Team1V2sbm3cApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Team1V2sbm3cApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Team1V2sbm3cApplication.class, args);
+    }
 
 }
