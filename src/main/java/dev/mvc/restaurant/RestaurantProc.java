7db4654519c00621593731ca4b478cf18d3760b7
박주창 <jooc0311@gmail.com>
2022-01-02 00:21:19 +0900

    가게 등록 페이지 구현

--- src/main/java/dev/mvc/restaurant/RestaurantProc.java
+++ src/main/java/dev/mvc/restaurant/RestaurantProc.java
@@ -1,6 +1,5 @@
 package dev.mvc.restaurant;
 
-import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,27 +7,23 @@ import org.springframework.stereotype.Component;
 
 import dev.mvc.members.MembersDAOInter;
 
+
 @Component("dev.mvc.restaurant.RestaurantProc")
 public class RestaurantProc implements RestaurantProcInter {
 
-	@Autowired
-	private RestaurantDAOInter restaurantDAO;
+    
+    @Autowired
+    private RestaurantDAOInter restaurantDAOInter;
+    
+    public RestaurantProc() {
+        System.out.println("-> restaurantProc created.");
+    }
+    
+    @Override
+    public int create(RestaurantVO restaurantVO) {
 
-	public RestaurantProc() {
-		System.out.println("-> RestaurantProc created.");
-	}
-
-	@Override
-	public List<RestaurantVO> testlist() {
-		List<RestaurantVO> testiList = this.restaurantDAO.testlist();
-
-		return testiList;
-	}
-
-	@Override
-	public int insert(RestaurantVO restaurantvo) {
-		int cnt = this.restaurantDAO.insert(restaurantvo);
-		return cnt;
-	}
-
+        int cnt = this.restaurantDAOInter.create(restaurantVO);
+        return cnt;
+    }
+    
 }
